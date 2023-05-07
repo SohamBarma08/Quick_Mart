e02ecdea168674a49e122f738097df6b2f8bd9f5
quickmart22 <csouvik50@gmail.com>
2023-05-07 21:19:34 +0530

    commit by souvik

--- tea_data.js
+++ tea_data.js
@@ -59,70 +59,63 @@ let shopItemsData = [
       price: 400,
       img: "images/tea/Raw_Pressery_Aloe_Vera_Lemonade_Drink.webp",
     },
+
+    {
+      id: "SunfeastStrawberryMilkshake",
+      name: "Sunfeast Strawberry Milkshake",
+      price: 37,
+      img: "images/tea/Sunfeast_Strawberry_Milkshake.avif",
+    },
+    {
+      id: "SunfeastDarkFantasyMilkshakewithBelgianChocolate",
+      name: "Sunfeast Dark Fantasy Milkshake with Belgian Chocolate",
+      price: 35,
+      img: "images/tea/Sunfeast_Dark_Fantasy_Milkshake_with_Belgian_Chocolate.avif",
+    },
+    {
+      id: "AmulLassi",
+      name: "Amul Lassi",
+      price: 17,
+      img: "images/tea/Amul_Lassi.avif",
+    },
+    {
+      id: "StingEnergyDrink",
+      name: "Sting Energy Drink",
+      price: 20,
+      img: "images/tea/Sting_Energy_Drink.webp",
+    },
+    {
+      id: "MonsterEnergyDrink",
+      name: "Monster Energy Drink",
+      price: 110,
+      img: "images/tea/Monster-Energy-Drink.webp",
+    },
+    {
+      id: "Nescafe_Intense_Cappuccino_Cold_Coffee",
+      name: "Nescafe Intense Cappuccino Cold Coffee Pack of 2",
+      price: 130,
+      img: "images/tea/Nescafe_Intense_Cappuccino_Cold-Coffee.avif",
+    },
+    {
+      id: "RedBullEnergyDrink",
+      name: "Red Bull Energy Drink",
+      price: 120,
+      img: "images/tea/redbull.webp",
+    },
+    {
+      id: "YakultProbioticHealthDrink",
+      name: "Yakult Probiotic Health Drink",
+      price: 150,
+      img: "images/tea/Yakult_Probiotic_Health_Drink.avif",
+    },
 ];
 
-//     <div class="box">
-//         <img src="images/tea/Yakult Probiotic Health Drink.avif" alt="potato">
-//         <span>Yakult Probiotic Health Drink</span>
-//         <h3 class="price">₹80<span>/5 piece</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Red Bull Energy Drink (250 ml).webp" alt="potato">
-//         <span>Red Bull Energy Drink</span>
-//         <h3 class="price">₹125<span>/250 ml</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Nescafe Intense Cappuccino Cold Coffee - Pack of 2.avif" alt="potato">
-//         <span>Nescafe Intense Cappuccino Cold Coffee Pack of 2</span>
-//         <h3 class="price">₹130<span>/2 piece</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Monster Energy Drink.webp" alt="potato">
-//         <span>Monster Energy Drink</span>
-//         <h3 class="price">₹110<span>/350ml</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Sting Energy Drink.webp" alt="potato">
-//         <span>Sting Energy Drink</span>
-//         <h3 class="price">₹20<span>/250ml</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Amul Lassi.avif" alt="Amul Lassi">
-//         <span>Amul Lassi</span>
-//         <h3 class="price">₹23<span>/250ml</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
-//     <div class="box">
-//         <img src="images/tea/Sunfeast Dark Fantasy Milkshake with Belgian Chocolate.avif" alt="potato">
-//         <span>Sunfeast Dark Fantasy Milkshake with Belgian Chocolate</span>
-//         <h3 class="price">₹37<span>/180ml</span>
-//             <h3>
-//                 <i class='bx bx-cart'></i>
-//                 <i class='bx bx-heart'></i>
-//     </div>
 
 //     <div class="box">
 //         <img src="images/tea/Sunfeast Strawberry Milkshake.avif" alt="potato">
